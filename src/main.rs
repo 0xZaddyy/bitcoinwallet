735cd21aee7508b55a353f72da5eb15f486f6de8
0xZaddyy <oyinoladapo5@gmail.com>
2024-08-18 11:34:50 +0100

    made few changes to code

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn generate_private_key() -> [u8; 32] {
     let mut private_key = [0u8; 32];
     rng.fill(&mut private_key);
     SecretKey::from_slice(&private_key).expect("32 bytes, within curve order");
-    // private_key
+    private_key
 
 } 
 
@@ -18,8 +18,8 @@ fn public_key_from_private_key(private_key: &[u8; 32]) -> [u8; 33] {
     
         let secp = Secp256k1::new();
         let secret_key = SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order");
-       let publicKey = publicKey::from_secret_key(&secp, &secret_key);
-
+       let publicKey = PublicKey::from_secret_key(&secp, &secret_key);
+ 
 
         let mut public_key = [0u8; 33];
     public_key[0] = 0x02; // Pretend to be a compressed public key
